d3d823c223062f0bace8d4e675f7d7097c569c50
Michael Nigh <mic.nigh@gmail.com>
2023-01-31 18:14:13 -0600

    cleanup admin view

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Label, Input, Checkbox } from 'theme-ui'
 
 const localStorage = typeof window !== 'undefined' ? window.localStorage : { getItem: () => null, setItem: () => null }
@@ -8,27 +8,31 @@ export const isEditMode = () => localStorage.getItem(getKey('edit-mode')) === 't
 export const getGithubToken = () => localStorage.getItem(getKey('github-token'))
 
 export const PageAdmin = () => {
+  const [displayGithubToken, setDisplayGithubToken] = useState(false)
   return (
     <div sx={{}}>
       <h2 sx={{ variant: 'styles.h2' }}>Admin</h2>
 
       <div sx={{
         mt: 3,
-        '& > *': {
-          mb: 3,
-        }
       }}>
-        <Label>
-          <Checkbox defaultChecked={localStorage.getItem(getKey('edit-mode')) === 'true'} onChange={e => localStorage.setItem(getKey('edit-mode'), JSON.stringify(e.target.checked))} />Edit Mode
+        <Label sx={{ mb: 2 }}>
+          <Checkbox sx={{ mr: 0 }} defaultChecked={localStorage.getItem(getKey('edit-mode')) === 'true'} onChange={e => localStorage.setItem(getKey('edit-mode'), JSON.stringify(e.target.checked))} />Edit Mode
         </Label>
-        <span>
+        <span sx={{ mb: 2 }}>
           <Label htmlFor='github-token' sx={{ fontWeight: 'bold' }}>Github token</Label>
-          <Input name='github-token' sx={{
-            fontSize: 1,
-            pt: 1,
-          }} type='text' onChange={e => {
-            localStorage.setItem(getKey('github-token'), e.target.value)
-          }} defaultValue={localStorage.getItem(getKey('github-token'))} />
+          <span sx={{ display: 'flex' }}>
+            <Input name='github-token' sx={{
+              fontSize: 1,
+              pt: 1,
+              flexGrow: 1,
+            }} type={displayGithubToken ? 'text' : 'password'} onChange={e => {
+              localStorage.setItem(getKey('github-token'), e.target.value)
+            }} defaultValue={localStorage.getItem(getKey('github-token'))} />
+            <Label sx={{ flexGrow: 0, width: 'auto', fontSize: 3, alignItems: 'center' }}>
+              <Checkbox checked={displayGithubToken} onChange={e => setDisplayGithubToken(!displayGithubToken)} sx={{ mr: 0, ml: 1 }} />Show
+            </Label>
+          </span>
         </span>
       </div>
     </div>
