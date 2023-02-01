094c1cbf369a1b80cc16d306902ce1b034322093
Michael Nigh <mic.nigh@gmail.com>
2023-02-01 08:18:06 -0600

    fix build

--- src/components/placepuppy/index.tsx
+++ src/components/placepuppy/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as puppyPlaceholders from './assets'
 
-export const PuppyPlaceholder = ({ index = 0, ...props }) => {
+export const PuppyPlaceholder = ({ index = 0, className = '' }) => {
   const PuppyImage = puppyPlaceholders[`puppy${(index % 27) + 1}`];
-  return <PuppyImage {...props} />
+  return <PuppyImage className={className} />
 }
