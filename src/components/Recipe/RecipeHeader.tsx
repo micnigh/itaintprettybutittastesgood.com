e282feffb707ce0ed0525e93b65d3260ce49da2f
Michael Nigh <mic.nigh@gmail.com>
2025-12-22 22:20:45 -0600

    add share button

--- src/components/Recipe/RecipeHeader.tsx
+++ src/components/Recipe/RecipeHeader.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { format } from 'date-fns'
 import type { Recipe } from '../../types/recipe'
 import { parseServingsFromMetadata } from '../../utils/recipe'
+import ShareButton from './ShareButton'
 
 interface RecipeHeaderProps {
   recipe: Recipe
@@ -12,29 +13,34 @@ const RecipeHeader: FC<RecipeHeaderProps> = ({ recipe }) => {
 
   return (
     <>
-      <div className="flex flex-row items-baseline space-x-2">
-        <span className="w-max">
-          <h1 className="text-3xl font-bold mt-4 sm:mt-6 mr-4">
-            {recipe.title}
-          </h1>
-          {recipe.metadata?.date && (
-            <div className="text-sm text-gray-500 mb-4">
-              {format(new Date(recipe.metadata.date), 'MMM d, yyyy')}
-            </div>
-          )}
-        </span>
-        {recipe.metadata?.tags && (
-          <span className="flex-grow min-w-48">
-            {recipe.metadata.tags.map((tag) => (
-              <span
-                key={tag}
-                className="bg-blue-400 text-white whitespace-nowrap rounded-full px-2 py-0.5 text-sm mr-4 mb-4 inline-block"
-              >
-                {tag}
-              </span>
-            ))}
+      <div className="flex flex-row items-start justify-between">
+        <div className="flex flex-row items-baseline space-x-2 flex-grow">
+          <span className="w-max">
+            <h1 className="text-3xl font-bold mt-4 sm:mt-6 mr-4">
+              {recipe.title}
+            </h1>
+            {recipe.metadata?.date && (
+              <div className="text-sm text-gray-500 mb-4">
+                {format(new Date(recipe.metadata.date), 'MMM d, yyyy')}
+              </div>
+            )}
           </span>
-        )}
+          {recipe.metadata?.tags && (
+            <span className="flex-grow min-w-48">
+              {recipe.metadata.tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="bg-blue-400 text-white whitespace-nowrap rounded-full px-2 py-0.5 text-sm mr-4 mb-4 inline-block"
+                >
+                  {tag}
+                </span>
+              ))}
+            </span>
+          )}
+        </div>
+        <div className="mt-4 sm:mt-6">
+          <ShareButton recipeTitle={recipe.title} />
+        </div>
       </div>
       <ul className="flex flex-row gap-4 mb-2">
         {recipe.metadata?.level && <li>Level: {recipe.metadata.level}</li>}
